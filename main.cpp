49e5c98ad34756b2593e7f8e77d569f01ef67b5a
lucialob93 <33631202+lucialob93@users.noreply.github.com>
2017-11-16 16:33:00 +0100

    Add files via upload
    This is the main file of the project

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include "mainwindow.h"
+#include <QApplication>
+#include <QDesktopWidget>
+#include <QMetaType>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    MainWindow w;
+    w.setFixedSize(w.width(),w.height()); // This will fix the size of the window.
+    w.setWindowFlags(w.windowFlags()^Qt::WindowMaximizeButtonHint); // This will disable the maximize button.
+    w.move(QApplication::desktop()->screen()->rect().center()-w.rect().center()); //This will shift the main window to the center of the screen.
+    w.show();
+    qRegisterMetaType<QVector<double> >("QVector<double>"); // Register the QVector<double> type before using it in the code.
+    return a.exec();
+}
