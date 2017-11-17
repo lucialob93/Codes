9e492936b422769f54435d6c02b20503b8f45b9a
lucialob93 <lucia.lobello@epfl.ch>
2017-11-17 10:45:02 +0100

    I have modified the files

--- main.cpp
+++ main.cpp
@@ -1,8 +1,8 @@
-#include "mainwindow.h"
+
 #include <QApplication>
 #include <QDesktopWidget>
 #include <QMetaType>
-
+// this is the program for..
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
